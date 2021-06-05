e3d36293e561dba786293083dc5d022973564d62
weemsj <71594052+weemsj@users.noreply.github.com>
2021-06-05 12:46:55 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('port', 8294);
+
+app.get('/',function(req,res){
+    res.type('text/plain');
+    res.send('Welcome to the main page!');
+});
+
+app.get('/other-page',function(req,res){
+    res.type('text/plain');
+    res.send('Welcome to the other page!');
+});
+
+app.use(function(req,res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
